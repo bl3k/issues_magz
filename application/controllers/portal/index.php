6914047e53c1a0d2a40f6f26133ff66e038d968f
kierjarat <kierjarat@gmail.com>
2015-02-28 22:57:05 +0700

    update index controller, publication model crud methodes

--- application/controllers/portal/index.php
+++ application/controllers/portal/index.php
@@ -13,4 +13,33 @@ class Index extends CI_Controller {
 		
 		$this->load->view('portal/home', $data);
 	}
+	
+	public function show() {
+		$id = 2;
+		
+		print_r($this->publication->get_record_by_id($id));
+	}
+	
+	public function do_insert() {
+		$data = array(
+			'name' => '<script>alert("Publication #1");</script>'
+		);
+		
+		echo $this->publication->insert($data);
+	}
+	
+	public function do_update() {
+		$id = 2;
+		$data = array(
+			'name' => '<script>alert("Publication #1 (edited)");</script>'
+		);
+		
+		echo $this->publication->update($id, $data);
+	}
+	
+	public function do_delete() {
+		$id = 2;
+		
+		echo $this->publication->delete($id);
+	}
 }
--- application/models/publication.php
+++ application/models/publication.php
@@ -10,4 +10,49 @@ class Publication extends CI_Model {
 		
 		return $result;
 	}
+	
+	public function get_record_by_id($id) {
+		$this->db->where($this->primary_key, $id);
+		
+		$result = $this->db->get($this->table_name);
+		
+		return $result->row_array();
+	}
+	
+	public function get_records_by_params(array $params) {
+		for ($i=0; $i < count($params); $i++) {
+			$this->db->where(key($params[$i]), $params[key($params[$i])]);
+		}
+		
+		$result = $this->db->get($this->table_name);
+		
+		return $result->result();
+	}
+	
+	public function insert(array $data) {
+		$this->db->insert_string($this->table_name, $data);
+		
+		$result = $this->db->affected_rows();
+		
+		return $result;
+	}
+	
+	public function update($id, array $data) {
+		$this->db->where($this->primary_key, $id);
+		$this->db->update($this->table_name, $data);
+		
+		$result = $this->db->affected_rows();
+		
+		return $result;
+		
+	}
+	
+	public function delete($id) {
+		$this->db->where($this->primary_key, $id);
+		$this->db->delete($this->table_name);
+		
+		$result = $this->db->affected_rows();
+		
+		return $result;
+	}
 }
